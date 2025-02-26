1af22e6ba3c19c995d4247f0cccadfbe5198d713
alessiozaccato <alessiozaccato@gmail.com>
2025-02-26 13:01:32 +0100

    ADD: fetch on port:3000

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
+  //let's create the dinamic array of posts
+  const [posts, SetPosts] = useState([]);
+
+  const fetchPosts = () => {
+    fetch('https://localhost:3000')
+      .then((res) => res.json())
+      .then((data) => SetPosts(data.results))
+
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(fetchPosts, []);
 
   return (
     <>
